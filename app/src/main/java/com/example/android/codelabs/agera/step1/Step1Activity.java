e81bd45967a4fe15de8d1075cee74ebd85c6d0b8
swami701 <swami701search@gmail.com>
2017-11-14 12:18:11 +0800

    BASIC IMPLEMENTATION: Basic implementation agera interfaces

--- app/src/main/java/com/example/android/codelabs/agera/step1/Step1Activity.java
+++ app/src/main/java/com/example/android/codelabs/agera/step1/Step1Activity.java
@@ -17,21 +17,52 @@
 package com.example.android.codelabs.agera.step1;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-
+import android.util.Log;
 import com.example.android.codelabs.agera.R;
-
+import com.google.android.agera.Observable;
+import com.google.android.agera.Receiver;
+import com.google.android.agera.Supplier;
+import com.google.android.agera.Updatable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Step1Activity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.step1);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.step1);
+    MyDataSupplier myDataSupplier = new MyDataSupplier();
 
+    // Updatable creation
+    Updatable updatable = () -> Log.i("AGERA", myDataSupplier.get());
+    myDataSupplier.addUpdatable(updatable);
+    myDataSupplier.accept("Hello Agera!");
+  }
+
+  private static class MyDataSupplier implements Observable, Supplier<String>, Receiver<String> {
+
+    List<Updatable> mUpdatables = new ArrayList<>();
+    private String mValue;
+
+    @Override public void addUpdatable(@NonNull Updatable updatable) {
+      mUpdatables.add(updatable);
     }
 
-//  private static class MyDataSupplier implements Observable, Supplier<String>, Receiver<String> {
+    @Override public void removeUpdatable(@NonNull Updatable updatable) {
+      mUpdatables.remove(updatable);
+    }
 
-//  }
+    @Override public void accept(@NonNull String value) {
+      mValue = value;
+      for (Updatable updatable : mUpdatables) {
+        updatable.update();
+      }
+    }
+
+    @NonNull @Override public String get() {
+      return mValue;
+    }
+  }
 }
